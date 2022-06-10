bb6c36be9c1455e47715126c14fff9f17810f9db
Lycanfloof <lyca2206@gmail.com>
2022-06-10 18:22:00 -0500

    Moar changes in modifier-experiments!

--- modifier-experiments/modifier.c
+++ modifier-experiments/modifier.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+// Here I experiment with what happens if you don't assign a value to a variable before printing it.
+
+int e;
+int f;
+int g;
+
 void main(void)
 {
-    //Here I experiment with what happens if you don't assign a value to a variable before printing it.
     int a;
     int b;
     int c;
     int d;
-    printf("%d\n", a + c);
+
+    printf("GLOBAL: %d\n", e);
+    printf("%d\n", f);
+    printf("%d\n", g);
+
+    printf("LOCAL: %d\n", a + c);
     printf("%d\n", a);
     printf("%d\n", b);
     printf("%d\n", c);
