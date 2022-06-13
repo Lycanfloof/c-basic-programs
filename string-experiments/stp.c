40ad9bad0a00035b58cd22c93e9b8e3796fe2ee1
Lycanfloof <lyca2206@gmail.com>
2022-06-13 13:21:11 -0500

    Change in stp.c

--- string-experiments/stp.c
+++ string-experiments/stp.c
@@ -3,7 +3,10 @@
 void main(void)
 {
     char *s = "Hello!";
+    // *s = 'A'; <- Error! The array has been defined as ROM, therefore you cannot change its value.
+    // You can change the pointer's value though.
     char a[] = {'W', 'o', 'r', 'l', 'd', '!'};
+    *a = 'A';
     printf("%s\n", s);
     printf("%s", a);
 }
