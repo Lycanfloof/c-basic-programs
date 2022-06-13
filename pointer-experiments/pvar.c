f6a7c2752fd1da368a4b6a454b6d7b8b4f812164
Lycanfloof <lyca2206@gmail.com>
2022-06-12 19:26:52 -0500

    Added pvar in pointer-experiments

---
+++ pointer-experiments/pvar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int a[2] = {1, 2};
+    int *p = a;
+
+    int b[2][3] = {1, 2, 3, 4};
+    int(*m)[3] = b;
+    int *q = *b;
+
+    int c[2][2][2] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int(*n)[2][2] = c;
+    int *r = **c;
+}
