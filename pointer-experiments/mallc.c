a372585aaf0d96782b32af86ec84fef87e174868
Lycanfloof <lyca2206@gmail.com>
2022-06-15 21:55:28 -0500

    Changes in mallc.c

--- pointer-experiments/mallc.c
+++ pointer-experiments/mallc.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 void main(void)
 {
+    char *p = NULL;
+    char *q = NULL;
+    char *r = NULL;
+    char *s = NULL;
+    char *t = NULL;
+
     // Memory allocation: It allocates a block of memory of a certain size and returns the address of the first byte.
-    char *p = malloc(5 * sizeof(char));
+    p = malloc(5 * sizeof(char));
 
     // Clean memory allocation: It allocates n blocks of memory of a certain size and returns the address of the first byte.
-    char *q = calloc(5, sizeof(char));
+    q = calloc(5, sizeof(char));
 
     // These two expressions allocate the same amount of memory.
     // malloc() doesn't set its values to NULL, while calloc() does. This means malloc() can return garbage values.
@@ -18,4 +25,23 @@ void main(void)
 
     free(q);
     q = NULL;
+
+    // Memory re-allocation: It allocates the given memory with a new size.
+    // If there's not enough room to add more blocks of memory, then it will copy the existing values into another location and add the new blocks.
+    // r = calloc(1, sizeof(char));
+    // s = calloc(1, sizeof(char));
+    r = malloc(16 * sizeof(char));
+    s = malloc(16 * sizeof(char));
+
+    t = r;
+    r = realloc(r, 64 * sizeof(char));
+
+    free(r);
+    r = NULL;
+
+    free(s);
+    s = NULL;
+
+    // free(t); <- This causes an exception because realloc() frees its memory after it's done copying it.
+    t = NULL;
 }
